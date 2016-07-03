1dc27b133fdc4bf16b3009a6588d07833191e7ee
Bill Shackelford <billshackelford@Bills-MacBook-Pro.local>
2016-07-02 22:59:43 -0400

    v1.0.15

--- dist/index.js
+++ dist/index.js
@@ -62,6 +62,8 @@
                 };
                 // this will hold the current url
                 this.url = '';
+                // this will hold the page title (not yet supported by browsers)
+                this.pageTitle = '';
                 // optional mediator placeholder
                 this.mediator = false;
             }
@@ -74,7 +76,7 @@
                     // set up the events
                     this.addListeners();
                     // navigate to correct view
-                    this.navigate();
+                    this.navigate(false, {}, false);
                     return this;
                 }
             }, {
@@ -89,68 +91,9 @@
                     //listen to a mediator if present
                     if (this.mediator) {
                         this.mediator.on('router:navigate', function (data) {
-                            _this2.navigate(data.url);
+                            _this2.navigate(data.url, data || {}, false);
                         });
                     }
-                    return this;
-                }
-            }, {
-                key: 'eventPushStateClick',
-                value: function eventPushStateClick(e) {
-                    e.preventDefault();
-                    this.url = e.target.getAttribute('href') || '';
-                    this.navigate();
-                    return this;
-                }
-            }, {
-                key: 'eventPopState',
-                value: function eventPopState(e) {
-                    this.url = e.state || '';
-                    this.navigate();
-                    return this;
-                }
-            }, {
-                key: 'navigate',
-                value: function navigate(url) {
-
-                    // default to false, status of if we have an route matching the url
-                    var matcher = false;
-
-                    // allow navigate to use a specified url
-                    if (url) {
-                        this.url = url;
-                    }
-
-                    // this is the object passed to the matching view
-                    this.locationData = {
-                        url: this.url,
-                        data: []
-                    };
-
-                    //find the first matcher that starts with the same string as a defined route
-                    for (var route in this.routes) {
-                        if (this.url.lastIndexOf(route, 0) === 0) {
-                            // parse out the url data
-                            this.locationData.data = this.url.replace(route, '').split('/');
-                            this.locationData.data.splice(0, 1);
-                            // remove any trailing empty items from the array
-                            if (this.locationData.data.length && this.locationData.data[this.locationData.data.length - 1] === '') {
-                                this.locationData.data.splice(this.locationData.data.length - 1, 1);
-                            }
-                            //execute the route
-                            this.routes[route](this.locationData);
-                            //set that we have found a match
-                            matcher = true;
-                            //stop looping we are done
-                            break;
-                        }
-                    }
-                    // else if a catch all default route is defined execute that
-                    if (matcher === false && this.routes.defaultRoute) {
-                        this.routes.defaultRoute(this.locationData);
-                    }
-                    //set in history the new url
-                    window.history.pushState(this.url, null, this.url);
                     return this;
                 }
             }, {
@@ -166,6 +109,70 @@
                     (0, _gator2.default)(document).off('click', '[data-pushstate]');
                     //bind window popstates
                     window.removeEventListener('popstate', this.eventPopState.bind(this));
+                    return this;
+                }
+            }, {
+                key: 'eventPushStateClick',
+                value: function eventPushStateClick(e) {
+                    e.preventDefault();
+                    this.url = e.target.getAttribute('href') || '';
+                    this.navigate(false, {}, false);
+                    return this;
+                }
+            }, {
+                key: 'eventPopState',
+                value: function eventPopState(e) {
+                    this.url = e.state || '';
+                    this.navigate(false, {}, true);
+                    return this;
+                }
+            }, {
+                key: 'navigate',
+                value: function navigate(url, data, isPopState) {
+
+                    // default to false, status of if we have an route matching the url
+                    var matcher = false;
+
+                    // allow navigate to use a specified url
+                    if (url) {
+                        this.url = url;
+                    }
+
+                    // this is the object passed to the matching view
+                    this.locationData = {
+                        url: this.url,
+                        data: {
+                            url: [],
+                            mediator: data
+                        }
+                    };
+
+                    //find the first matcher that starts with the same string as a defined route
+                    for (var route in this.routes) {
+                        if (this.url.lastIndexOf(route, 0) === 0) {
+                            // parse out the url data
+                            this.locationData.data.url = this.url.replace(route, '').split('/');
+                            this.locationData.data.url.splice(0, 1);
+                            // remove any trailing empty items from the array
+                            if (this.locationData.data.url.length && this.locationData.data.url[this.locationData.data.url.length - 1] === '') {
+                                this.locationData.data.url.splice(this.locationData.data.url.length - 1, 1);
+                            }
+                            //execute the route
+                            this.routes[route](this.locationData);
+                            //set that we have found a match
+                            matcher = true;
+                            //stop looping we are done
+                            break;
+                        }
+                    }
+                    // else if a catch all default route is defined execute that
+                    if (matcher === false && this.routes.defaultRoute) {
+                        this.routes.defaultRoute(this.locationData);
+                    }
+                    if (!isPopState) {
+                        //set in history the new url
+                        window.history.pushState(this.url, this.pageTitle, this.url);
+                    }
                     return this;
                 }
             }]);
