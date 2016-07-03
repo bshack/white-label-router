7049d265159c5a4a5f99ff6befdf2cb083cf895d
William Shackelford <bshackelford@gmail.com>
2016-07-02 22:56:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import Gator from 'gator';
             };
             // this will hold the current url
             this.url = '';
+            // this will hold the page title (not yet supported by browsers)
+            this.pageTitle = '';
             // optional mediator placeholder
             this.mediator = false;
         }
@@ -22,42 +24,45 @@ import Gator from 'gator';
             // set up the events
             this.addListeners();
             // navigate to correct view
-            this.navigate();
+            this.navigate(false, {}, false);
             return this;
         }
         addListeners() {
             //bind all pushstate links
-            Gator(document).on(
-                'click',
-                '[data-pushstate]',
-                this.eventPushStateClick.bind(this)
-            );
+            Gator(document).on('click','[data-pushstate]', this.eventPushStateClick.bind(this));
             //bind window popstates
-            window
-                .addEventListener(
-                    'popstate',
-                    this.eventPopState.bind(this)
-                );
+            window.addEventListener('popstate', this.eventPopState.bind(this));
             //listen to a mediator if present
             if (this.mediator) {
                 this.mediator.on('router:navigate', (data) => {
-                    this.navigate(data.url);
+                    this.navigate(data.url, (data || {}), false);
                 });
             }
+            return this;
+        }
+        destroy() {
+            this.removeListeners();
+            return this;
+        }
+        removeListeners() {
+            //unbind all pushstate links
+            Gator(document).off('click', '[data-pushstate]');
+            //bind window popstates
+            window.removeEventListener('popstate', this.eventPopState.bind(this));
             return this;
         }
         eventPushStateClick(e) {
             e.preventDefault();
             this.url = e.target.getAttribute('href') || '';
-            this.navigate();
+            this.navigate(false, {}, false);
             return this;
         }
         eventPopState(e) {
             this.url = (e.state || '');
-            this.navigate();
+            this.navigate(false, {}, true);
             return this;
         }
-        navigate(url) {
+        navigate(url, data, isPopState) {
 
             // default to false, status of if we have an route matching the url
             let matcher = false;
@@ -70,19 +75,22 @@ import Gator from 'gator';
             // this is the object passed to the matching view
             this.locationData = {
                 url: this.url,
-                data: []
+                data: {
+                    url: [],
+                    mediator: data
+                }
             };
 
             //find the first matcher that starts with the same string as a defined route
             for (let route in this.routes) {
                 if (this.url.lastIndexOf(route, 0) === 0) {
                     // parse out the url data
-                    this.locationData.data = this.url.replace(route, '').split('/');
-                    this.locationData.data.splice(0, 1);
+                    this.locationData.data.url = this.url.replace(route, '').split('/');
+                    this.locationData.data.url.splice(0, 1);
                     // remove any trailing empty items from the array
-                    if (this.locationData.data.length
-                            && this.locationData.data[this.locationData.data.length - 1] === '') {
-                        this.locationData.data.splice(this.locationData.data.length - 1, 1);
+                    if (this.locationData.data.url.length
+                            && this.locationData.data.url[this.locationData.data.url.length - 1] === '') {
+                        this.locationData.data.url.splice(this.locationData.data.url.length - 1, 1);
                     }
                     //execute the route
                     this.routes[route](this.locationData);
@@ -96,23 +104,10 @@ import Gator from 'gator';
             if (matcher === false && this.routes.defaultRoute) {
                 this.routes.defaultRoute(this.locationData);
             }
-            //set in history the new url
-            window.history.pushState(this.url, null, this.url);
-            return this;
-        }
-        destroy() {
-            this.removeListeners();
-            return this;
-        }
-        removeListeners() {
-            //unbind all pushstate links
-            Gator(document).off('click', '[data-pushstate]');
-            //bind window popstates
-            window
-                .removeEventListener(
-                    'popstate',
-                    this.eventPopState.bind(this)
-                );
+            if (!isPopState) {
+                //set in history the new url
+                window.history.pushState(this.url, this.pageTitle, this.url);
+            }
             return this;
         }
     };
